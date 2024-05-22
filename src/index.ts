4abf8d76136cacd9f4556eb3768abca8f5e228bb
keyanmaskoot <keyanmaskoot@gmail.com>
2024-05-22 15:25:43 +0200

    initial implementation of `monitor` endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,69 @@
+// @ts-ignore
 import * as packageJson from "../package.json";
-import { Command } from "commander";
-import kleur from "kleur";
+import { Command } from "@commander-js/extra-typings";
+import * as kleur from "kleur";
+import { Lucid, fetchBatchRequestUTxOs } from "@anastasia-labs/smart-handles-offchain";
 
-const program = new Command();
+type Target = "Single" | "Batch" | "Both";
+
+const program: Command = new Command();
+
+program.version(packageJson.version).description(packageJson.description);
 
 program
-  .version(packageJson.version)
-  .description(packageJson.description);
-
-program
-  .command("echo <text>")
-  .alias("e")
-  .description("Echo the provided text")
-  .action((text) => {
+  .command("monitor")
+  .alias("m")
+  .description(
+    "Start monitoring the Minswap smart handles address, and perform the routing to collect their fees"
+  )
+  .option(
+    "-t, --target <VARIANT>",
+    "Specify smart handle variant",
+    (initV: string): Target => {
+      const v = initV.toLowerCase();
+      if (v == "single") {
+        return "Single";
+      } else if (v == "both") {
+        return "Both";
+      } else {
+        return "Batch";
+      }
+    },
+    "Batch"
+  )
+  .option(
+    "-i, --polling-interval <MILLISECONDS>",
+    "Specify the polling interval in milliseconds",
+    (x: string, def: number): number => {
+      const parsed = parseInt(x);
+      if (isNaN(parsed)) {
+        console.log(kleur.yellow(`WARNING: Bad polling interval, reverting to default (${def}ms)`));
+        return def;
+      } else {
+        return parsed;
+      }
+    },
+    10000
+  )
+  .option("-t, --testnet", "Switch to preprod testnet", false)
+  .action(({ target, pollingInterval, testnet }) => {
     console.log("");
-    console.log(kleur.green(text));
-    console.log("");
+    console.log(
+      kleur.bold(
+        `Monitoring Minswap smart handles script for ${kleur.blue(
+          target == "Single"
+            ? "Single"
+            : target == "Both"
+            ? "both Single and Batch"
+            : "Batch"
+        )} requests\u2026`
+      )
+    );
+    const lucid = new Lucid();
+    console.log(kleur.dim(`Polling every ${pollingInterval}`));
+    setInterval(async () => {
+      await fetchBatchRequestUTxOs(lucid, testnet);
+    }, pollingInterval);
   });
 
 program.parse();
